f9637ae8ee1c7e51dd5d1368eb944773271e3fd7
aaug <aaug@caasco.ca>
2021-09-23 22:57:49 -0400

    browser sync serves files from dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,64 @@
-let defaultTask = (cb) => {
-    console.log("this is the defailt task this")
-}
+/**
+ * Settings
+ * Turn on/off build features
+ */
 
-exports.default = defaultTask
+var settings = {
+    reload: true
+};
+
+/**
+ * Paths to project folders
+ */
+
+var paths = {
+    input: 'src/',
+    output: 'dist/',
+    reload: './dist/'
+};
+
+// General
+var {gulp, watch, series} = require('gulp');
+
+// BrowserSync
+var browserSync = require('browser-sync');
+
+// Watch for changes to the src directory
+var startServer = function (done) {
+
+    // Make sure this feature is activated before running
+    if (!settings.reload) return done();
+
+    // Initialize BrowserSync
+    browserSync.init({
+        server: {
+            baseDir: paths.reload
+        }
+    });
+
+    // Signal completion
+    done();
+
+};
+
+// Reload the browser when files change
+var reloadBrowser = function (done) {
+    if (!settings.reload) return done();
+    browserSync.reload();
+    done();
+};
+
+// Watch for changes
+var watchSource = function (done) {
+    // watch(paths.input, series(exports.default, reloadBrowser));
+    watch(paths.input, series(reloadBrowser));
+    done();
+};
+
+// Watch and reload
+// gulp watch
+exports.watch = series(
+    // exports.default,
+    startServer,
+    watchSource
+);
